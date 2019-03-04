b56a27f8b70a24051755666c3b29e320acff4b4a
Julien Ledun <j.ledun@iosystems.fr>
2019-03-04 18:26:54 +0100

    first tested encoding / decoding a modbus communication with Siemens S7-1200 : success
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const flattenBDF = (bdf) => {
     tmp = tmp.concat(
       (field.hasOwnProperty('properties')) ? flattenBDF(field.properties) : 
       (field.hasOwnProperty('array')) ? 
-        flattenBDF(field.array).map(
+        field.array.flatMap(
           arrayItem => flattenBDF(arrayItem)
         ) :
       field
@@ -40,35 +40,41 @@ const flattenBDF = (bdf) => {
   return tmp;
 }
 
-const flattenData = (data, bdf) => {
-  let tmp = [];
-  data.values().forEach(key => {
-    è
-  });
-  return tmp;
+const getBDFByteLength = bdf => {
+  const lastDef = flattenBDF(bdf).pop();
+  return lastDef.offset + getFieldLength(lastDef);
 }
 
 const getReadFunction = (plctype, options) => {
+  return (plctype.buftype.indexOf('toString') > -1) ?
+    plctype.buftype :
+    "".concat(
+      "read",
+      getTypeFunction(plctype, options)
+    );
+}
+
+const getWriteFunction = (plctype, options) => {
   return "".concat(
-    "read",
+    "write",
     getTypeFunction(plctype, options)
   );
 }
 
-const getWriteFunction = (plctype, options) => {
-}
-
 const getTypeField = (plctype) => {
-  return typeDefinitions.find(
-    typedef => plctype.type.indexOf(typedef.plctype) > -1
-  );
+  const fieldtype = plctype.type.replace(/ARRAY OF /gi, "");
+  return typeDefinitions.find(typedef => typedef.plctype === fieldtype);
 }
 
 const getTypeFunction = (plctype, options) => {
-  return "".concat(
-    getTypeField(plctype).buftype,
-    options.endianness
-  );
+  return (plctype.buftype.indexOf("8") > -1) ?
+    "".concat(
+      getTypeField(plctype).buftype
+    ) :
+    "".concat(
+      getTypeField(plctype).buftype,
+      options.endianness
+    );
 }
 
 const getFieldLength = (plctype) => {
@@ -84,6 +90,7 @@ const checkTypeField = (plctype) => {
   if (!plctype.hasOwnProperty('offset')) throw new Error(`missing offset`, plctype);
   if (typeof getTypeField(plctype) === "undefined") throw new Error(`Type does not exist`, plctype);
   if (plctype.type.indexOf('BOOL') > -1 && !plctype.hasOwnProperty('bitnumber')) throw new Error(`missing 'bitnumber' field`, plctype);
+  if (plctype.type.indexOf('BOOL') > -1 && (plctype.bitnumber < 0 || plctype.bitnumber > 15)) throw new Error(`wrong 'bitnumber', it should be between 0 and 15`, plctype);
   if (plctype.type.indexOf('STRING') > -1 && (!plctype.hasOwnProperty('length') || plctype.length <= 0)) throw new Error(`missing or null length`, plctype);
   if (plctype.type.indexOf('ARRAY') > -1 && (!plctype.hasOwnProperty('length') || plctype.length <= 0)) throw new Error(`missing or null length`, plctype);
   if (plctype.type.indexOf('ARRAY') > -1 && plctype.type.indexOf('STRING') > -1) throw new Error(`ARRAY OF STRING or ARRAY OF S7STRING are not implemented yet :-(`, plctype);
@@ -122,21 +129,29 @@ const checkBuffer = (buffer, bdf) => {
   // check buffer
   if (buffer === null || buffer === undefined) throw new Error(`Nothing to decode`);
   if (!Buffer.isBuffer(buffer)) throw new Error(`buffer is not a Buffer`);
-  const lastDef = flattenBDF(bdf).pop();
-  const totalLength = lastDef.offset + getFieldLength(lastDef);
+  const totalLength = getBDFByteLength(bdf);
   if (buffer.length !== totalLength) throw new Error(`buffer length is not what it should be.`);
 }
 
 const init = (defPath) => {
   return new Promise(async (resolve, reject) => {
     try{
-      const bdf = await getFileDescription(defPath);
+      const bdf = (typeof defPath === "string") ? 
+        await getFileDescription(defPath) : 
+        (Array.isArray(defPath)) ? [...defPath] : null;
+      if (!bdf) throw new Error(`The first parameter must be a string or an array.`);
       checkBDFTypes(bdf);
       return resolve(bdf);
     }catch(e){
       return reject(e);
     }
   });
+}
+
+const getBitNumber = (bn) => {
+  if (bn < 0 || bn > 15) throw new Error(`wrong bit number`, field);
+  const bitNumbers = [0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100, 0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000];
+  return bitNumbers[bn];
 }
 
 const readFieldFromBuffer = (buffer, field, options) => {
@@ -149,12 +164,15 @@ const readFieldFromBuffer = (buffer, field, options) => {
     case "S7STRING":
       let offset = tmp.offset;
       let stringObject = {};
-      stringObject.maxlength = buffer[getReadFunction(buftype.maxlength, options)](offset);
-      offset += buftype.maxlength.length;
-      stringObject.length = buffer[getReadFunction(buftype.length, options)](offset);
-      offset += buftype.length.length;
-      stringObject.value = buffer[getReadFunction(buftype.value, options)]('ascii', offset, offset + stringObject.length);
+      stringObject.maxlength = buffer[getReadFunction(tmp.buftype.maxlength, options)](offset);
+      offset += tmp.buftype.maxlength.length;
+      stringObject.length = buffer[getReadFunction(tmp.buftype.length, options)](offset);
+      offset += tmp.buftype.length.length;
+      stringObject.value = buffer[getReadFunction(tmp.buftype.value, options)]('ascii', offset, offset + stringObject.length);
       return stringObject;
+
+    case "BOOL":
+      return Boolean(buffer[getReadFunction(tmp, options)](tmp.offset) & getBitNumber(tmp.bitnumber));
 
     default:
       return buffer[getReadFunction(tmp, options)](tmp.offset);
@@ -170,7 +188,8 @@ const createBDFForArrayOf = (field) => {
       ...field,
       ...{
         type: arrayType.plctype,
-        offset: offset
+        offset: offset,
+        length: arrayType.length
       }
     });
     offset += arrayType.length;
@@ -180,7 +199,7 @@ const createBDFForArrayOf = (field) => {
 
 const readArrayOfFieldFromBuffer = (buffer, field, options) => {
   const arraybdf = createBDFForArrayOf(field);
-  return {[field.name]: arraybdf.map(arraybdfitem => readFromBuffer(buffer, arraybdfitem, options))};
+  return arraybdf.map(arraybdfitem => readFromBuffer(buffer, arraybdfitem, options));
 }
 
 const readFromBuffer = (buffer, field, options) => {
@@ -194,10 +213,14 @@ const parseBuffer = (buffer, bdf, options) => {
   let tmp = {};
   bdf.forEach(field => {
     if (field.hasOwnProperty('properties')) {
-      tmp = {...tmp, ...parseBuffer(buffer, field.properties, options)};
+      tmp = {...tmp, ...{[field.name]: (tmp.hasOwnProperty(field.name)) ? 
+        {...tmp[field.name], ...parseBuffer(buffer, field.properties, options)}: 
+        parseBuffer(buffer, field.properties, options)}};
 
     }else if (field.hasOwnProperty('array')) {
-      tmp = {...tmp, ...{[field.name]: field.array.map(arrayField => parseBuffer(buffer, arrayField, options))}};
+      tmp = {...tmp, ...{[field.name]: (tmp.hasOwnProperty(field.name)) ?
+        tmp[field.name].concat(field.array.map(arrayField => parseBuffer(buffer, arrayField, options))) :
+        field.array.map(arrayField => parseBuffer(buffer, arrayField, options))}};
 
     }else{
       tmp = {...tmp, ...{[field.name]: readFromBuffer(buffer, field, options)}};
@@ -205,6 +228,95 @@ const parseBuffer = (buffer, bdf, options) => {
     }
   });
   return tmp;
+}
+
+const mergeDataInBDF = (data, bdf) => {
+  const tmp = bdf.map(bf => {
+    if (bf.hasOwnProperty('properties')) {
+      bf.properties = mergeDataInBDF((data && data.hasOwnProperty(bf.name)) ? data[bf.name] : {}, bf.properties);
+      return bf;
+
+    }else if (bf.hasOwnProperty('array')) {
+      bf.array = bf.array.map((arrItem, index) => {
+        return mergeDataInBDF((data && data.hasOwnProperty(bf.name)) ? data[bf.name] : {}, arrItem);
+      });
+      return bf;
+
+    }else{
+      bf.value = (data && data.hasOwnProperty(bf.name)) ? data[bf.name] :  bf.default;
+      return bf;
+
+    }
+  });
+  return tmp;
+}
+
+const createBuffer = (data, bdf, options) => {
+  const flattened = flattenBDF(mergeDataInBDF(data, bdf)).map(bf => ({...getTypeField(bf), ...bf}));
+  let buf = Buffer.alloc(getBDFByteLength(bdf));
+  let byteOffset = -1;
+  let byteValue = 0;
+  let boolValues = [];
+  let tmpType = {};
+  flattened.forEach(bf => {
+    switch(bf.plctype) {
+      case "CHAR":
+      case "STRING":
+        buf.write((Array.isArray(bf.value) ? bf.value.join() : bf.value), bf.offset, bf.length, 'ascii');
+        break;
+
+      case "S7STRING":
+        let offset = bf.offset;
+        bf.value.length = bf.value.value.length;
+        buf[getWriteFunction(bf.buftype.maxlength, options)](bf.value.maxlength, offset);
+        offset += bf.buftype.maxlength.length;
+        buf[getWriteFunction(bf.buftype.length, options)](bf.value.length, offset);
+        offset += bf.buftype.length.length;
+        buf.write(bf.value.value, offset, bf.value.length, 'ascii');
+        break;
+
+      case "BOOL":
+        if (byteOffset !== -1 && byteOffset !== bf.offset) {
+          const index = boolValues.findIndex(bv => bv.offset === tmpType.offset);
+          if (index < 0) {
+            tmpType.value += byteValue;
+            boolValues.push(tmpType);
+          }else{
+            boolValues[index].value += byteValue;
+          }
+          tmpType = {};
+          byteOffset = -1;
+        }
+        if (byteOffset === -1) {
+          byteOffset = bf.offset;
+          byteValue = 0
+          tmpType = {
+            ...bf,
+            ...getTypeField({type: "WORD"}),
+            ...{value: byteValue}
+          };
+        }
+        byteValue += (bf.value) ? getBitNumber(bf.bitnumber) : 0;
+        break;
+
+      default:
+        buf[getWriteFunction(bf, options)](bf.value, bf.offset);
+        break;
+    }
+  });
+  if (byteOffset > -1) {
+    const index = boolValues.findIndex(bv => bv.offset === tmpType.offset);
+    if (index < 0) {
+      tmpType.value += byteValue;
+      boolValues.push(tmpType);
+    }else{
+      boolValues[index].value += byteValue;
+    }
+  }
+  boolValues.forEach(bv => {
+    buf[getWriteFunction(bv, options)](bv.value, bv.offset);
+  });
+  return buf;
 }
 
 const BufferWorker = {
@@ -216,10 +328,9 @@ const BufferWorker = {
       const defPath = bufferDescriptionFilePath;
       const opt = {...BufferWorker.defaultOptions, ...options};
       try{
-        console.log(data);
         const bdf = await init(defPath);
         checkData(data, bdf);
-        return resolve(bdf);
+        return resolve(createBuffer(data, bdf, opt));
       }catch(e){
         return reject(e);
       }
@@ -232,7 +343,7 @@ const BufferWorker = {
       try{
         const bdf = await init(defPath);
         checkBuffer(buffer, bdf);
-        return resolve(parseBuffer(buffer, bdf, options));
+        return resolve(parseBuffer(buffer, bdf, opt));
       }catch(e){
         return reject(e);
       }
